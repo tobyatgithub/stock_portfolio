667b412e96f3de81b69c2be1c75fb73ff09a56d4
tobyatgithub <toby.fangyuan@gmail.com>
2018-12-06 16:39:48 -0800

    version that works

---
+++ src/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+import os
+
+# get root of the project. The benefit is path is not hard coded and shall be adaptable.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# create app
+app = Flask(
+    __name__,
+    static_url_path='',
+    static_folder='static',
+    instance_relative_config= True # allows config to be in the flask app
+)
+
+app.config.from_mapping(
+    SECRET_KEY = os.getenv('SECRET_KEY'),
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL'),
+    # SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL'),
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+)
+
+# mont config from source directory
+from . import routes, forms, models, exceptions
---
+++ src/wsgi.py
@@ -0,0 +1,5 @@
+from . import app
+
+# here we handles the run part----let server run.
+if __name__ =='__main__':
+    app.run()
